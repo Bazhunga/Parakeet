90c578921724874cdcc25c037ffcc892a75b016b
JesseChua94 <jessechua131@gmail.com>
2015-05-09 02:57:42 -0400

    added some labels

--- Parakeet/app/src/main/java/com/medical/parakeet/parakeet/Main_PatientDetail.java
+++ Parakeet/app/src/main/java/com/medical/parakeet/parakeet/Main_PatientDetail.java
@@ -1,6 +1,7 @@
 package com.medical.parakeet.parakeet;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,134 +13,56 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class Main_PatientDetail extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class Main_PatientDetail extends ActionBarActivity{
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+    View decorView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
+        //Puts screen in immersive sticky mode
+        decorView = getWindow().getDecorView();
+        stickyImmersion(decorView);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main__patient_detail);
-        getSupportActionBar().hide();
+/*
+        TextView pName = (TextView) findViewById(R.id.pName);
+        pName.setTextColor(Color.rgb(200, 0, 0));
+        pName.setText(pName.getText().toString() + "Mary Jones");
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+        TextView dob = (TextView) findViewById(R.id.dob);
+        dob.setText(dob.getText().toString() + "10/10/10");
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        TextView age = (TextView) findViewById(R.id.age);
+        age.setText(age.getText().toString() + "32");
+
+        TextView gender = (TextView) findViewById(R.id.gender);
+        gender.setText(gender.getText().toString() + "Female");
+
+        TextView aType = (TextView) findViewById(R.id.aType);
+        aType.setText(aType.getText().toString() + "Inpatient"); */
+
+        TextView pProfileText = (TextView) findViewById(R.id.pProfileText);
+        pProfileText.setText("The patient is a 32 year old female. " +
+                "The patient has symptoms of abdominal pain. Refer to note in patient chart for" +
+                "documentation of history and physical. previously obtained ct showed: stones found in the" +
+                "gallbladder. the patient has failed previous conservative treatment. laparoscopy is " +
+                "recommended due to the patient's progressive symptoms. the altnernatives, risks and" +
+                "benefits of surgery were discussed with the patient. THe patient verbalized" +
+                "understanding of the risks as well as the alternatives to surgery. The patient wished" +
+                "to proceed with operative intervention");
     }
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main__patient_detail, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((Main_PatientDetail) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+    public void stickyImmersion(View decorView) {
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
 }
