0fc5679eacfbc513245cdd6313042f326b3ffead
Vanshil Shah <mr.vanshilshah@gmail.com>
2015-05-09 10:48:38 -0400

    Revert "scrollview not working"
    This reverts commit cd497f56f55a326fc4057de45c5c0b0207582616.

--- Parakeet/app/src/main/java/com/medical/parakeet/parakeet/Main_PatientDetail.java
+++ Parakeet/app/src/main/java/com/medical/parakeet/parakeet/Main_PatientDetail.java
@@ -1,7 +1,6 @@
 package com.medical.parakeet.parakeet;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,15 +13,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main_PatientDetail extends ActionBarActivity{
@@ -38,13 +31,32 @@ public class Main_PatientDetail extends ActionBarActivity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main__patient_detail);
+/*
+        TextView pName = (TextView) findViewById(R.id.pName);
+        pName.setTextColor(Color.rgb(200, 0, 0));
+        pName.setText(pName.getText().toString() + "Mary Jones");
 
-        LinearLayout scroll = (LinearLayout) findViewById(R.id.textview);
-        LayoutInflater inflater =  (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-       // View view = inflater.inflate(R.layout.textview, null);
-        TextView view = new TextView(this.getApplicationContext());
-        scroll.addView(view);
+        TextView dob = (TextView) findViewById(R.id.dob);
+        dob.setText(dob.getText().toString() + "10/10/10");
 
+        TextView age = (TextView) findViewById(R.id.age);
+        age.setText(age.getText().toString() + "32");
+
+        TextView gender = (TextView) findViewById(R.id.gender);
+        gender.setText(gender.getText().toString() + "Female");
+
+        TextView aType = (TextView) findViewById(R.id.aType);
+        aType.setText(aType.getText().toString() + "Inpatient"); */
+
+        TextView pProfileText = (TextView) findViewById(R.id.pProfileText);
+        pProfileText.setText("The patient is a 32 year old female. " +
+                "The patient has symptoms of abdominal pain. Refer to note in patient chart for" +
+                "documentation of history and physical. previously obtained ct showed: stones found in the" +
+                "gallbladder. the patient has failed previous conservative treatment. laparoscopy is " +
+                "recommended due to the patient's progressive symptoms. the altnernatives, risks and" +
+                "benefits of surgery were discussed with the patient. THe patient verbalized" +
+                "understanding of the risks as well as the alternatives to surgery. The patient wished" +
+                "to proceed with operative intervention");
     }
 
     public void stickyImmersion(View decorView) {
